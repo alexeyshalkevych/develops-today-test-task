528313ebbe99aedcd0a1568225cd68344e0138b7
Alexey <valnistuy@gmail.com>
2020-09-10 19:52:27 +0300

    :docs: create structure project and configs

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from 'next';
+import Head from 'next/head';
+
+const Index: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Develops Today Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Index page ...</h1>
+    </div>
+  );
+};
+
+export default Index;
